008246a7c91a0e82884ab4a25a44cbf6186e36af
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-18 00:18:59 +0200

    Add files via upload

---
+++ Lambda and LINQ - Lab/Largest3Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split(' ');
+
+          
+            var numbers = strings.Select(int.Parse).ToList().OrderByDescending(x => x).Take(3);
+            Console.WriteLine(String.Join(" ",numbers));
+        }
+    }
+}
---
+++ Lambda and LINQ - Lab/ShortWordsSorted.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = new char[]
+  {'.',',',':',';','(',')','[',']','\\','\"','\'','/','!','?',' '};
+
+            var list = Console.ReadLine().ToLower()
+                .Split(separators)
+                .ToArray()
+                .Distinct()
+                .OrderBy(x => x).
+                Where(l => l != "")
+                .Where(c => c.Length<5);
+
+            Console.WriteLine(String.Join(", ", list));
+
+
+
+        }
+    }
+}
+       
