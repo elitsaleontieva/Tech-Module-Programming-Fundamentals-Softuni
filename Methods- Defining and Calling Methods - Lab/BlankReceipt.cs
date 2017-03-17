4d4e8ad05e44435ddde7a2458b45d68b02fb702d
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 21:51:03 +0200

    Update and rename DEMO to BlankReceipt.cs

---
+++ Methods- Defining and Calling Methods - Lab/BlankReceipt.cs
@@ -0,0 +1,38 @@
+
+using System;
+
+
+namespace BlankReceipt
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+
+
+        }
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+    }
+
+}
+   
