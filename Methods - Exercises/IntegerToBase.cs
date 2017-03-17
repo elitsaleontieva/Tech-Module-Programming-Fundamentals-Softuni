40f5663b6175f19857bf913aae5e4257d9a81622
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 22:17:58 +0200

    Create IntegerToBase.cs

---
+++ Methods - Exercises/IntegerToBase.cs
@@ -0,0 +1,31 @@
+using System;
+
+
+namespace IntegerToBase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var toBase = int.Parse(Console.ReadLine());
+            var converted = IntegerToBase(number, toBase);
+            Console.WriteLine(converted);
+        }
+
+
+        static string IntegerToBase(int number, int toBase)
+        {
+            var result = string.Empty;
+            while (number > 0)
+            {
+
+                var remainder = number % toBase;
+                result = remainder + result;
+                number /= toBase;
+
+            }
+            return result;
+        }
+    }
+}
