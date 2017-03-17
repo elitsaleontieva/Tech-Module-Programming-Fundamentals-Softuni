6a64e35837b48a90b1da1eb450ebd5201244cb62
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-18 00:07:12 +0200

    Update LetterRepetition.cs

--- Dictionaries - Exercises/LetterRepetition.cs
+++ Dictionaries - Exercises/LetterRepetition.cs
@@ -10,9 +10,7 @@ namespace LetterRepetition
     {
         static void Main(string[] args)
         {
-            //You will be given a single string, 
-            //    containing random ASCII character.Your task is to print every character, 
-            //and how many times it has been used in the string
+          
 
             var inputString = Console.ReadLine();
             var dictionary = new Dictionary<char,int>();
