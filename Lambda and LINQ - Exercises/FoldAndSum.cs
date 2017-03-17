689ed353ce2cde897cc3b9abaa721f2e9caeb940
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-18 00:22:43 +0200

    Update and rename concatenate.cs to FoldAndSum.cs

---
+++ Lambda and LINQ - Exercises/FoldAndSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlattenDictionart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                  var numbers = Console.ReadLine().Split().Select(int.Parse)
+                .ToArray();
+
+            var k = numbers.Length / 4;
+
+            var firstPartUpperRow = numbers.Take(k).Reverse().ToList();
+            var secondPartUpperRow = numbers.Reverse().Take(k);
+
+            var upperRow = firstPartUpperRow.Concat(secondPartUpperRow).ToArray();
+            var lowerRow = numbers.Skip(k).Take(2 * k).ToArray();
+
+            var result = new int[upperRow.Length];
+
+            for (int i = 0; i < upperRow.Length; i++)
+            {
+                result[i] = upperRow[i] + lowerRow[i];
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+
+        }
+    }
+}
