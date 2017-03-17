5794a1ee7092ebb95120667d9b5fdf1622579bcc
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 20:55:50 +0200

    Update and rename specialnumbersforeach.cs to SpecialNumbersFirstSolution.cs

---
+++ Data Types- Text and Other Types- Variables - Lab/SpecialNumbersFirstSolution.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+namespace SpecialNumbersFirstSolution
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+		
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int number = num;
+
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+		    
+                bool special = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine("{0} -> {1}", num, special);
+            }
+        }
+    }
+}
