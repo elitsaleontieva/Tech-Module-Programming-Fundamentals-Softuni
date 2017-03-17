0d3051c19cc0d5da436a97b5ddebc1790215f113
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 22:13:45 +0200

    Create HelloName.cs

---
+++ Methods - Exercises/HelloName.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace HelloName
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+
+
+
+        }
+    }
+}
+  
