6535624fef3a205c923fa1e3cd5cd86cd4cb51e8
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 20:53:35 +0200

    Update PyramidVolume.cs

--- Data Types- Text and Other Types- Variables - Lab/PyramidVolume.cs
+++ Data Types- Text and Other Types- Variables - Lab/PyramidVolume.cs
@@ -8,14 +8,18 @@ namespace PyramidVolume
 		public static void Main(string[] args)
 		{
 			double length, width, height = 0;
-Console.Write("Length: ");
-length = double.Parse(Console.ReadLine());
-Console.Write("Width: ");
-width = double.Parse(Console.ReadLine());
-Console.Write("Height: ");
-height = double.Parse(Console.ReadLine());
-double volume = (length * width * height) / 3;
-Console.WriteLine("Pyramid Volume: {0:F2}", volume); 
+			
+			Console.Write("Length: ");
+			length = double.Parse(Console.ReadLine());
+			
+			Console.Write("Width: ");
+			width = double.Parse(Console.ReadLine());
+			
+			Console.Write("Height: ");
+			height = double.Parse(Console.ReadLine());
+			
+			double volume = (length * width * height) / 3;
+C		onsole.WriteLine("Pyramid Volume: {0:F2}", volume); 
 		}
 	}
 }
