913d253f6ae567195bceaaccf3af724a74f30d7e
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-18 00:08:43 +0200

    Add files via upload

---
+++ Dictionaries - Lab/CountRealNumberss.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumberss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var counts = new SortedDictionary<double, int>();
+
+            var count = 0;
+            foreach (var num in nums)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+
+
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+
+
+
+
+
+            }
+
+            foreach (var i in counts.Keys)
+            {
+                Console.WriteLine("{0} -> {1}", i, counts[i]);
+
+
+            }
+        }
+    }
+}
