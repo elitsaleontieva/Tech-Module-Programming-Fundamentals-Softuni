04eba14b625a3bdf581a874b6d301a5bfdf477d2
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 23:45:10 +0200

    Update Ununion.cs

--- Lists - More Exercises/Ununion.cs
+++ Lists - More Exercises/Ununion.cs
@@ -1,11 +1,4 @@
-﻿/*
- * 
- * 
- * 
- * 
- * 
- * 
- */
+
 using System;
 using System.Collections.Generic;	
 using System.Linq;
@@ -15,12 +8,7 @@ namespace Ununion
 	{
 		public static void Main(string[] args)
 		{
-				//You will be given a sequence of integers, separated by a space. This is the primal list. 
-		//Then you will receive an integer N. On the next N lines, you will receive sequences of integers. 
-		//Your task is to add all elements that the primal list does not contain from the current sequence to the primal list
-		//and then remove from the primal list, all elements which are contained in the current sequence of integers. If there are several occurrences,
-		//remove all occurrences you find.
-//After you are done receiving lists and manipulating the primal list, sort the primal list and print it.
+				
 
 		  var primal = Console.ReadLine().Split().Select(int.Parse).ToList();
  
