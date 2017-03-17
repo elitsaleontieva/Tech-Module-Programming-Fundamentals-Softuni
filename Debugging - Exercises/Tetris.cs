b45f366006e40888399760c305737bc7e80e81b7
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 22:30:50 +0200

    Create Tetris.cs

---
+++ Debugging - Exercises/Tetris.cs
@@ -0,0 +1,101 @@
+using System;
+
+
+
+namespace Tetris
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            string currentDirection = Console.ReadLine();
+
+            while (currentDirection != "exit")
+            {
+                switch (currentDirection)
+                {
+                    case "left":
+                        Left(n);
+                        break;
+
+                    case "down":
+                        Down(n);
+                        break;
+
+                    case "right":
+                        Right(n);
+                        break;
+
+                    case "up":
+                        Up(n);
+                        break;
+                }
+                currentDirection = Console.ReadLine();
+            }
+        }
+
+        public static void Left(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('.', n) + new string('*', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 2 * n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('.', n) + new string('*', n));
+            }
+        }
+
+        public static void Right(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', n) + new string('.', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 2 * n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', n) + new string('.', n));
+            }
+        }
+
+        public static void Up(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('.', n) + new string('*', n) + new string('.', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', n * 3));
+            }
+        }
+
+        public static void Down(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', n * 3));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('.', n) + new string('*', n) + new string('.', n));
+            }
+        }
+    }
+}
