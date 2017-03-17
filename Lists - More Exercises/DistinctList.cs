4738b496151b16518a15bda74adb34511bc1b8ad
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 23:44:27 +0200

    Update DistinctList.cs

--- Lists - More Exercises/DistinctList.cs
+++ Lists - More Exercises/DistinctList.cs
@@ -1,11 +1,4 @@
-﻿/*
- * 
- * 
- * 
- * 
- * 
- * 
- */
+
 using System;
 using System.Collections.Generic;	
 using System.Linq;	
@@ -15,7 +8,7 @@ namespace DistinctList
 	{
 		public static void Main(string[] args)
 		{
-			//You will be given a list of integers on the first line of the input (space-separated). Remove all repeating elements from the list.
+			
 		var list1=Console.ReadLine().Split(' ').Select(int.Parse).ToList();
 		
 		Console.WriteLine(String.Join(" ",list1.Distinct()));
