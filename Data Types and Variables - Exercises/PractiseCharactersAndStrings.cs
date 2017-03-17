d4de05df748fa0242ed9bc5ef7589f1138da6b88
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 21:33:06 +0200

    Create PractiseCharactersAndStrings.cs

---
+++ Data Types and Variables - Exercises/PractiseCharactersAndStrings.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace PracticeCharactersAndStrings
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            char b = 'B';
+            char y = 'y';
+            char e = 'e';
+            String string1 = "I love programming";
+            String string2 = "Software University";
+
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n", string2, b, y, e, string1);
+        }
+    }
+}
