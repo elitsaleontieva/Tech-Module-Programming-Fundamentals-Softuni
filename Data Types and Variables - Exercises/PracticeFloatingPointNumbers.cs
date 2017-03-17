551cc7c602a943215eced7f57ed1d507f663a9f2
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 21:15:45 +0200

    Create PracticeFloatingPointNumbers.cs

---
+++ Data Types and Variables - Exercises/PracticeFloatingPointNumbers.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace PracticeFloatingPointNumbers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("3.141592653589793238");
+            Console.WriteLine("1.60217657");
+            Console.WriteLine("7.8184261974584555216535342341");
+
+        }
+    }
+}
