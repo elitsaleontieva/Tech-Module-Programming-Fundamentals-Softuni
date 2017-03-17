72a78a7f54cfceecabee69792c36308912094b5a
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 22:41:19 +0200

    Create SmallestNumberInArray.cs

---
+++ Arrays - Simple Array Processing - Lab/SmallestNumberInArray.cs
@@ -0,0 +1,37 @@
+using System;
+
+
+
+namespace SmallestNumberInArray
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stringArray = Console.ReadLine().Split();
+           
+            var array = new int[stringArray.Length];
+
+
+
+            for (int i = 0; i < array.Length; i++)
+
+
+                array[i] = int.Parse(stringArray[i]); 
+
+
+            int smallestNumber = int.MaxValue;
+
+            for (int i = 0; i < array.Length; i++)
+
+                if (array[i] < smallestNumber)
+                {  
+                    smallestNumber = array[i];
+
+                }
+            Console.WriteLine(smallestNumber); 
+
+
+        }
+    }
+}
