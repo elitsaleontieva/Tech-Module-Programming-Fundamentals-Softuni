b22a6bef9429a71895f566111bde792264453932
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 23:36:43 +0200

    Add files via upload

--- Lists - Exercises/EqualSumAfterExtraction.cs
+++ Lists - Exercises/EqualSumAfterExtraction.cs
@@ -1,55 +1,60 @@
+﻿/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 using System;
-using System.Collections.Generic;
+	using System.Collections.Generic;	
 using System.Linq;
-
-
 namespace EqualSumAfterExtraction
-
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            var list1 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            var list2 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-
-
-            for (int i = 0; i < list1.Count; i++)
-            {
-
-                for (int j = 0; j < list2.Count; j++)
-                {
-
-                    if (list1[i] == list2[j])
-                    {
-                        list2.RemoveAll(item => item == list1[i]);
-                    }
-                }
-            }
-
-
-
-
-
-
-
-            if ((list1.Sum()) == (list2.Sum()))
-            {
-
-                Console.WriteLine("Yes. Sum: {0}", list1.Sum());
-
-
-            }
-
-            else
-            {
-
-                Console.WriteLine("No. Diff: {0}", (Math.Abs((list1.Sum()) - (list2.Sum()))));
-
-            }
-
-
-        }
-    }
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		//You will be given two integer lists on the first two lines (space-separated). 
+		////Remove the elements in the first list from the elements in the second list. If an element from the first 
+			//list occurs more than once in the second list, remove all occurrences.
+//After you remove the elements, check the sum of both lists. 
+		//If the sum of both of them is equal, print “Yes. Sum: {sum}”. If not print the absolute difference between the two lists in the format “No. Diff: {sum}”.
+		var list1=Console.ReadLine().Split(' ').Select(int.Parse).ToList(); 
+		var list2=Console.ReadLine().Split(' ').Select(int.Parse).ToList(); 
+		
+		
+		for(int i=0; i<list1.Count ; i++){
+				
+			for(int j=0; j<list2.Count; j++){
+				
+				if(list1[i]==list2[j]){
+			list2.RemoveAll(item => item ==list1[i]);
+				}
+			}
+		}
+				
+				
+			
+				
+			
+		
+			 
+			if((list1.Sum())==(list2.Sum())){ 
+				
+				Console.WriteLine("Yes. Sum: {0}",list1.Sum());
+			
+				
+			}
+			
+			else{ 
+				
+				Console.WriteLine("No. Diff: {0}",(Math.Abs((list1.Sum())-(list2.Sum()))));
+				
+			}
+			
+			
+		}
+	}
 
 }
---
+++ Lists - Exercises/FlipListSides.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * You will receive an integer list on the first line of the input (space-separated). 
+* Leave the first, middle and last elements as they are. For every other element, 
+* exchange it with its opposite indexed element (list[1]  list[length-2] and so on…).
+* After that, print the list on the console (space-separated).
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;	
+using System.Linq;
+namespace FlipListSides
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+ 
+            List<int> firstPart = new List<int>();
+            List<int> secondPart = new List<int>();
+            List<int> result = new List<int>();
+ 
+            int firstNum = input[0];
+            int lastNum = input[input.Count - 1];
+            int middleNum = input[input.Count / 2];
+ 
+            for (int i = 0; i < input.Count / 2 - 1; i++)
+            {
+                firstPart.Add(input[i + 1]);
+            }
+ 
+            firstPart.Reverse();
+            input.Reverse();
+ 
+            for (int i = 0; i < input.Count / 2 - 1; i++)
+            {
+                secondPart.Add(input[i + 1]);
+            }
+ 
+            if (input.Count ==1)
+            {
+                Console.WriteLine(firstNum);
+            }
+            else if (input.Count == 2)
+            {
+                Console.WriteLine(firstNum + " " + lastNum);
+            }
+            else if (input.Count == 3)
+            {
+                Console.WriteLine($"{firstNum} {middleNum} {lastNum}");
+            }
+            else if (input.Count % 2 != 0)
+            {
+                Console.WriteLine(firstNum + " " + (string.Join(" ", secondPart)) + " " + middleNum + " " + (string.Join(" ", firstPart)) + " " + lastNum);
+            }
+            else
+            {
+                Console.WriteLine(firstNum + " " + (string.Join(" ", secondPart)) + " " + (string.Join(" ", firstPart)) + " " + lastNum);
+            }
+        }
+    }
+}
