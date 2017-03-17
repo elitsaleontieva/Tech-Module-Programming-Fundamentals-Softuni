b3869c4166282e84f54fd7d3f20eb3ce8c2306b7
elitsaleontieva <ellyleontieva@abv.bg>
2017-03-17 21:56:33 +0200

    Create CalculateTriangleArea.cs

---
+++ Methods- Defining and Calling Methods - Lab/CalculateTriangleArea.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace CalculateTriangleArea
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetTriangleArea(width, height);
+
+            Console.WriteLine(area);
+        }
+
+        static double GetTriangleArea(double width, double height)
+        {
+
+            return width * height / 2;
+        }
+    }
+}
