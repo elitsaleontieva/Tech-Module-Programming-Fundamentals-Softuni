a0a0da7648d6cf93845613fa1ede7427df692bb7
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-03-30 15:44:10 +0300

    Add files via upload

---
+++ Files- Directories and Exceptions/MergeFiles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstFile = File.ReadAllText("FileOne.txt").Split();
+            string[] secondFile = File.ReadAllText("FileTwo.txt").Split('\r', '\n');
+            File.WriteAllText("result.txt", "");
+
+            for (int i = 0; i < firstFile.Length; i++)
+            {
+                File.AppendAllText("result.txt", firstFile[i] + "\r\n" + secondFile[i]);
+            }
+
+
+
+
+
+        }
+
+    }
+}
