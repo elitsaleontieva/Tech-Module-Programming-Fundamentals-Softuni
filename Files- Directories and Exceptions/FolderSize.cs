5eb308df21c6edbe57b5a64ecd4264fcb57db558
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-03-30 16:01:06 +0300

    Add files via upload

---
+++ Files- Directories and Exceptions/FolderSize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filesInDirectory = Directory.GetFiles("TestFolder");
+            double sizeOfFilesTotal = 0;
+
+            foreach (string file in filesInDirectory)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sizeOfFilesTotal += fileInfo.Length;
+            }
+
+            sizeOfFilesTotal = sizeOfFilesTotal / 1024 / 1024;
+
+            File.WriteAllText("оutput.txt", sizeOfFilesTotal.ToString());
+
+        }
+    }
+}
