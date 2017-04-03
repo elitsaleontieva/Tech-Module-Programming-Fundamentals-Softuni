a27a227135aafc2826c2e717670e4df89a764570
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-03 16:40:55 +0300

    Add files via upload

---
+++ Strings and Text Processing/TextFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bannedWords = Console.ReadLine().Split(' ', ',');
+            var input = Console.ReadLine();
+
+            foreach (var banned in bannedWords)
+            {
+                if (input.Contains(banned))
+                {
+                    if (banned.Length > 0)
+                    {
+                        input = input.Replace(banned, new String('*', banned.Length));
+                    }
+                    }
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
