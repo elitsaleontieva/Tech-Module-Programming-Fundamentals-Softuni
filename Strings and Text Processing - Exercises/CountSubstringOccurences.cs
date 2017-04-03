71197e2e543e02563093b955f88bc1f8640d4186
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-03 21:17:50 +0300

    Add files via upload

---
+++ Strings and Text Processing - Exercises/CountSubstringOccurences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSubstringOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
+            var indexOf = input.IndexOf(pattern);
+
+            var count = 0;
+            for(int i=0; i<input.Length; i++)
+            {
+                if (indexOf != -1) {
+                    indexOf = (input.IndexOf(pattern, indexOf + 1));
+
+                count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Strings and Text Processing - Exercises/Palindromes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Palindromes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] {'-','>'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new SortedSet<string>();
+
+            foreach (var str in input)
+            {
+                if (IsPalindrome(str))
+                {
+                    result.Add(str);
+                }
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+
+        public static bool IsPalindrome(string value)
+        {
+            var reversed = value.ToCharArray().Reverse();
+
+            reversed = string.Join("", reversed);
+
+            if (value.Equals(reversed))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+
