7355253a0e77ab269e295d725ca3d61fd2c10a5e
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-06 22:25:37 +0300

    Add files via upload

---
+++ Regular Expressions -RegEx- - Lab/ReplaceATag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ReplaceATag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            while (text != "end")
+            {
+                string pattern =@"<a.*?href.*?=(.*)>(.*?)<\/a>";
+                string replace = @"[URL href=$1]$2[/URL]";
+                string replaced = Regex.Replace(text, pattern, replace);
+                Console.WriteLine(replaced);
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
