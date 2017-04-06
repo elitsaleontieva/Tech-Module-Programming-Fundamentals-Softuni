7cfd15ed51be55d6d769e254175ee11eb6e9ddaf
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-06 16:19:30 +0300

    Add files via upload

---
+++ Strings - More Exercise/SerializeString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var splitInput = input.ToCharArray().ToList();
+            var distinct = splitInput.Distinct().ToList();
+            var list = new List<string>();
+
+            for(int i=0; i<distinct.Count; i++)
+            {
+               
+                for (int j = 0; j < splitInput.Count; j++)
+                {
+                    if (distinct[i].Equals(splitInput[j]))
+                    {
+                        list.Add(j.ToString());
+                        
+                    }
+                }
+                Console.Write(($"{distinct[i]}:"));
+                Console.WriteLine($"{String.Join("/", list)}");
+                list.Clear();
+            }
+
+           
+        }
+    }
+}
---
+++ Strings - More Exercise/Stateless.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stateless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+            while (command != "collapse")
+            {
+                var input = command;
+                var exceptions = Console.ReadLine();
+
+                while (exceptions.Length > 0)
+                {
+                    input = input.Replace(exceptions, "").Trim();
+
+                    exceptions = exceptions.Remove(0, 1); //removes the first letter
+
+
+                    if (exceptions.Length > 0)
+                    {
+                        exceptions = exceptions.Remove(exceptions.Length - 1, 1); //removes the last letter
+                    }
+
+                }
+
+                if (input.Equals(String.Empty))
+                {
+                    Console.WriteLine("(void)");
+                }
+
+                Console.WriteLine(input);
+ 
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
+
+   
+
