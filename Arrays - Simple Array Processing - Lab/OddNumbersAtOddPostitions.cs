49d7dad64fa90ca17c1b65864c81328af754baaf
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-07 15:18:10 +0300

    Create OddNumbersAtOddPostitions.cs

---
+++ Arrays - Simple Array Processing - Lab/OddNumbersAtOddPostitions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            var count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2!=0 && i%2!=0)
+                {
+                    count++;
+
+                    Console.WriteLine($"Index {i} -> {array[i]}");
+                }
+            }
+            
+        }
+    }
+}
