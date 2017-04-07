9b4e4e0c2ecccc1f6743134440bdb4379820abb3
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-07 14:57:29 +0300

    Update MultiplyanArrayofDoubles.cs

--- Arrays - Simple Array Processing - Lab/MultiplyanArrayofDoubles.cs
+++ Arrays - Simple Array Processing - Lab/MultiplyanArrayofDoubles.cs
@@ -1,33 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-
-namespace MultiplyanArrayofDoubles
+namespace arrays
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var stringArray = Console.ReadLine().Split();
-            
-            var multiplication = new double[stringArray.Length];
+            var array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var n = double.Parse(Console.ReadLine());
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= n;
 
-
-            for (int i = 0; i < multiplication.Length; i++)
-
-
-                multiplication[i] = double.Parse(stringArray[i]); //string to double
-            double p = double.Parse(Console.ReadLine());
-
-            for (int i = 0; i < multiplication.Length; i++)
-                multiplication[i] *= p;
-
-
-
-            for (int i = 0; i < multiplication.Length; i++)
-
-                Console.Write(multiplication[i] + " ");
+            }
+            Console.WriteLine(String.Join(" ",array));
 
         }
     }
