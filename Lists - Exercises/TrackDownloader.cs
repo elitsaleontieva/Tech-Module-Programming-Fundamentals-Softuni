ac85ae0ad1edbe3426ef368999b001f118d94248
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-04-08 12:01:14 +0300

    Update TrackDownloader.cs

--- Lists - Exercises/TrackDownloader.cs
+++ Lists - Exercises/TrackDownloader.cs
@@ -1,40 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace TrackDownloader
-
+namespace ConsoleApplication10
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var blackList = Console.ReadLine().Split();
-            var input = Console.ReadLine();
-            var newList = new List<string>();
+
+                var blackList = Console.ReadLine().Split();
+                var input = Console.ReadLine();
+                var newList = new List<string>();
+
+           
             while (input != "end")
-            {
-                bool inBlackList = false;
+                {
+
+                var inBlackList = false;
+
                 for (int i = 0; i < blackList.Length; i++)
-                {
-                    if (input.Contains(blackList[i]))
                     {
-                        inBlackList = true;
+                        if (input.Contains(blackList[i]))
+                        {
+                            inBlackList = true;
+                        }
+
                     }
-                }
 
-                if (inBlackList != true)
-                {
-                    newList.Add(input);
-                }
+                    if (inBlackList != true)
+                    {
+                        newList.Add(input);
+                    }
+
                 input = Console.ReadLine();
-            }
+                }
+               
+                Console.WriteLine(String.Join($"{Environment.NewLine}", newList.OrderBy(x => x)));
 
-            foreach (var list in newList.OrderBy(x => x))
-            {
-                Console.WriteLine(list);
+            
             }
         }
     }
-}
