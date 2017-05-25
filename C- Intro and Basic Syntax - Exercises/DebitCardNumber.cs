f00478fc6ac5d0546ca45fd9ecf716932c69a3d8
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-05-25 13:10:20 +0300

    Create DebitCardNumber.cs

---
+++ C- Intro and Basic Syntax - Exercises/DebitCardNumber.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var num1 =int.Parse(Console.ReadLine());
+            var num2= int.Parse(Console.ReadLine());
+            var num3 =int.Parse(Console.ReadLine());
+            var num4 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"{num1.ToString("D4")} {num2.ToString("D4")} {num3.ToString("D4")} {num4.ToString("D4")}");
+            
+
+        }
+    }
+}
